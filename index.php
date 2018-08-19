17f889a9ab74a16cd472cb02e1f0b99655ad0f42
V Know <imvknow@gmail.com>
2018-08-19 13:59:53 -0400

    image change

--- index.php
+++ index.php
@@ -304,26 +304,26 @@
 					</div>						
 					<div class="row">
 						<div class="col-lg-4">
-							<a href="img/g1.jpg" class="img-pop-home">
-								<img class="img-fluid" src="img/g1.jpg" alt="">
+							<a href="img/new/Virjin_Mojito.jpg" class="img-pop-home">
+								<img class="img-fluid" src="img/new/Virjin_Mojito2.jpg" alt="">
 							</a>	
-							<a href="img/g2.jpg" class="img-pop-home">
-								<img class="img-fluid" src="img/g2.jpg" alt="">
+							<a href="img/new/Cheesy_Bun_Omelette.jpg" class="img-pop-home">
+								<img class="img-fluid" src="img/new/Cheesy_Bun_Omelette2.jpg" alt="">
 							</a>	
 						</div>
 						<div class="col-lg-8">
-							<a href="img/g3.jpg" class="img-pop-home">
-								<img class="img-fluid" src="img/g3.jpg" alt="">
+							<a href="img/new/Brownie_Icecream.jpg" class="img-pop-home">
+								<img class="img-fluid" src="img/new/Brownie_Icecream.jpg" alt="">
 							</a>	
 							<div class="row">
 								<div class="col-lg-6">
-									<a href="img/g4.jpg" class="img-pop-home">
-										<img class="img-fluid" src="img/g4.jpg" alt="">
+									<a href="img/new/Masala_Tea.jpg" class="img-pop-home">
+										<img class="img-fluid" src="img/new/Masala_Tea.jpg" alt="">
 									</a>	
 								</div>
 								<div class="col-lg-6">
-									<a href="img/g5.jpg" class="img-pop-home">
-										<img class="img-fluid" src="img/g5.jpg" alt="">
+									<a href="img/new/Masala_Maggie.jpg" class="img-pop-home">
+										<img class="img-fluid" src="img/new/Masala_Maggie.jpg" alt="">
 									</a>	
 								</div>
 							</div>
